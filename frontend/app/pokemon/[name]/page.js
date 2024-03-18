e36635c7fdad3b73c65dff3004d2deb71fe6e5c8
JacobElbaz <jacobel@post.bgu.ac.il>
2024-03-18 19:57:14 +0200

    add charts

--- frontend/app/pokemon/[name]/page.js
+++ frontend/app/pokemon/[name]/page.js
@@ -2,6 +2,7 @@
 import PokemonCard from "@/components/PokemonCard";
 import { useFetch } from "@/hooks/useFetch";
 import React from "react";
+import { Line, LineChart, Tooltip } from "recharts";
 
 function PokemonPage({ params }) {
   const { data, isPending, error } = useFetch(
@@ -19,6 +20,10 @@ function PokemonPage({ params }) {
         <div className="pokemon-page">
           <PokemonCard pokemon={data} single/>
           <p>Rank: {data.rank}</p>
+          <LineChart width={200} height={200} data={data.rankings}>
+            <Line type="monotone" dataKey={'rank'} stroke="#8884d8"/>
+            <Tooltip/>
+          </LineChart>
         </div>
       )}
     </>
