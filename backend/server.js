d242d2a327b85d22185906518d74c55f3e9b3d9e
JacobElbaz <jacobel@post.bgu.ac.il>
2024-03-20 19:06:33 +0200

    deploy

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,13 @@ require('./jobs');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const proxyDepth = parseInt(process.env.ADAPTABLE_TRUST_PROXY_DEPTH, 10);
+if (proxyDepth > 0) {
+    // 'trust proxy' is the number of IP addresses to trust in the
+    // X-Forwarded-For header, so set to the number of proxies plus one for the
+    // client IP address.
+    app.set('trust proxy', proxyDepth + 1);
+}
 
 app.use(cors());
 app.use(bodyParser.json())
